513100e933af6fa3bd075772fb6d07179d6ded60
aldoBangun <punk.aldo68@gmail.com>
2022-06-09 12:54:26 +0800

    Add task-1.js

---
+++ task-1.js
@@ -0,0 +1,61 @@
+const biodata = {
+   name: 'Alfredo Putra Djaru Bangun',
+   age: 24,
+   hobbies: ['Games', 'Music', 'Movies', 'Coding'],
+   isMaried: false,
+   schoolList: [
+      {
+         name: 'SDI ENDE 13',
+         yearIn: 2004,
+         yearOut: 2007,
+         major: null
+      },
+      {
+         name: 'SDI LASIANA',
+         yearIn: 2007,
+         yearOut: 2009,
+         major: null
+      },
+      {
+         name: 'SMPN 10 KUPANG',
+         yearIn: 2009,
+         yearOut: 2012,
+         major: null
+      },
+      {
+         name: 'SMKN 2 KUPANG',
+         yearIn: 2012,
+         yearOut: 2015,
+         major: 'Multimedia'
+      },
+      {
+         name: 'Politeknik Negeri Kupang',
+         yearIn: 2015,
+         yearOut: 2019,
+         major: 'Computer and Network Engineering'
+      }
+   ],
+   skills: [
+      {
+         name: 'Frontend Web Development',
+         level: 'Intermediate'
+      },
+      {
+         name: 'Backend Web Development',
+         level: 'Beginner'
+      },
+      {
+         name: 'UI/UX Design',
+         level: 'Beginner'
+      },
+      {
+         name: 'English Language',
+         level: 'Beginner'
+      },
+      {
+         name: 'Sitting on Front of Computer',
+         level: 'Advanced'
+      }
+   ],
+   interesInCoding: true
+}
